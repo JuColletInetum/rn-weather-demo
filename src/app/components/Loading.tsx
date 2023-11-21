ec1828acfff29aa6d1c7728f01dc08adae8e9bd4
Julien Collet <julien@mbp-de-julien.lan>
2023-11-21 21:57:01 +0100

    fix (loading) -> add native animation

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,34 +1,48 @@
 import {useEffect, useState, useRef} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Animated, Easing} from 'react-native';
 import {Images} from '../../common/assets';
 import {size} from '../../common/primitives';
 import colors from '../../common/primitives/colors';
 
 const Loading = () => {
-  const [rotation, setRotation] = useState(0);
-  const timer = useRef<NodeJS.Timeout>();
+  const rotation = useRef(new Animated.Value(0)).current;
+
+  const interpolateRotation = rotation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
+  const animatedStyle = {
+    transform: [{rotate: interpolateRotation}],
+  };
 
   useEffect(() => {
-    timer.current = setInterval(() => {
-      setRotation(prev => (prev <= 360 ? prev + 5 : 0));
-    }, 3);
-    return () => clearInterval(timer.current);
+    Animated.loop(
+      Animated.timing(rotation, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    ).start();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Images.Loading
-        width={size[3]}
-        height={size[3]}
-        color={colors.fontColor}
-        style={{
-          transform: [
-            {
-              rotate: `${rotation}deg`,
-            },
-          ],
-        }}
-      />
+      <Animated.View style={animatedStyle}>
+        <Images.Loading
+          width={size[3]}
+          height={size[3]}
+          color={colors.fontColor}
+          style={{
+            transform: [
+              {
+                rotate: `${rotation}deg`,
+              },
+            ],
+          }}
+        />
+      </Animated.View>
     </View>
   );
 };
